d64d9fb518d117730924ae284311277858df92c5
Igor Kovryzhkin <IgorKovr@gmail.com>
2017-05-23 11:26:11 +0300

    Initial Commit

---
+++ TensorFlowTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TensorFlowTest
+//
+//  Created by Igor Kovryzhkin on 5/23/17.
+//  Copyright © 2017 Igor Kovryzhkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
